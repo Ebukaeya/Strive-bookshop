43748583c261add019130080dfbe1634b6b1ef93
Ebuka <eyaebuka@gmail.com>
2022-03-16 23:17:28 +0100

    completed-D-3

--- striverestaurant/src/components/LatestRelease.jsx
+++ striverestaurant/src/components/LatestRelease.jsx
@@ -1,53 +1,53 @@
 import myData from "../data";
 import { Container, Row, Col } from "react-bootstrap";
+import { Component } from "react";
+import SingleBook from "./SingleBook";
 
-const LatestRelease = () => {
- 
-  return (
-    <>
-      <Container className="mt-3">
-        <h5 className="mb-5">Latest Books</h5>
-        <Row>
-          {myData.map((book) =>
-          
-            <Col className="mb-5" key={book.asin} sm={12} md={4} lg={3}>
-              <div   className="cardWrapper">
-                <div className="imageDiv">
-                  <img
-                    src={book.img}
-                    height="100%"
-                    width="100%"
-                    style={{ objectFit: "cover" }}
-                  ></img>
-                </div>
-                <div className="content">
-                  <div>
-                    <p>
-                      <strong>category</strong>
-                    </p>
-                    <p>
-                      <strong>Price</strong>
-                    </p>
-                  </div>
-                  <div>
-                    <p>{book.category}</p>
-                    <p>{book.price}</p>
-                  </div>
-                </div>
-                <div className="button">
-                  <button>Buy</button>
-                </div>
-              </div>
-            </Col>
-          
-           )}
+let data = myData;
 
-           
-         
-        </Row>
-      </Container>
-    </>
-  );
-};
+class LatestRelease extends Component {
+  state = {
+    fliterValue: "",
+  };
+
+  filtere = (e) => {
+    this.setState({ fliterValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <Container className="mt-3">
+          <div className="searchBar">
+            {" "}
+            <h5 className="mb-5">Latest Books</h5>
+            <input
+              placeholder="Search book"
+              onChange={(e) => {
+                this.filtere(e);
+              }}
+              value={this.state.fliterValue}
+            />
+          </div>
+          <Row>
+            {data
+              .filter((b) =>
+                b.title.toLowerCase().includes(this.state.fliterValue)
+              )
+              .map((book) => (
+                <SingleBook
+                  key={book.asin}
+                  asin={book.asin}
+                  img={book.img}
+                  category={book.title}
+                  price={book.price}
+                />
+              ))}
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
 
 export default LatestRelease;
